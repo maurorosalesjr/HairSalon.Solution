172e210fb4b490133eaa241cde72700a4bdc92d5
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-07-31 16:51:15 -0700

    file restructure and reupload of schema

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HairSalon.Models
+{
+  public class HairSalonContext : DbContext
+  {
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
